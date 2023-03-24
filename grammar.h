8005ea59b2c74a6b28eab67b7c8db7ab13895b72
kale <42823429+oelin@users.noreply.github.com>
2023-03-24 14:39:19 +0000

    Update grammar.h

--- grammar.h
+++ grammar.h
@@ -3,47 +3,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define Match(l) treeConstruct( \
-        TreeTypeProposition, (Tree *) (l), NULL)
+#define Match(l) treeConstruct(TreeTypeProposition, (Tree *)(l), NULL)
 
-#define And(l, r) treeConstruct( \
-        TreeTypeConjunction, l, r)
+#define And(l, r) treeConstruct(TreeTypeConjunction, l, r)
 
-#define Or(l, r) treeConstruct( \
-        TreeTypeDisjunction, l, r)
+#define Or(l, r) treeConstruct(TreeTypeDisjunction, l, r)
 
-#define Map(l, r) treeConstruct( \
-        TreeTypeImplication, l, r)
+#define Map(l, r) treeConstruct(TreeTypeImplication, l, r)
 
-#define Parse(s, x) treeConstruct( \
-        TreeTypeProposition, (Tree *) (s), (Tree *) (x))
+#define Parse(s, x) treeConstruct(TreeTypeProposition, (Tree *)(s), (Tree *)(x))
 
-
-#define Variable(name) Tree *(name) = (Tree *) calloc(1, sizeof(Tree));
+#define Variable(name) Tree *(name) = (Tree *)calloc(1, sizeof(Tree));
 
 #define Production(name, x) memcpy(name, x, sizeof(Tree));
 
-
 typedef enum {
-        TreeTypeProposition,
-        TreeTypeConjunction,
-        TreeTypeDisjunction,
-        TreeTypeImplication
+  TreeTypeProposition,
+  TreeTypeConjunction,
+  TreeTypeDisjunction,
+  TreeTypeImplication
 } TreeType;
-
 
 typedef struct Tree Tree;
 
 struct Tree {
-        Tree *left;
-        Tree *right;
-        TreeType type;
+  Tree *left;
+  Tree *right;
+  TreeType type;
 };
-
 
 Tree *treeConstruct(TreeType, Tree *, Tree *);
 void treeDeconstruct(Tree *);
-
 
 Tree *parseTree(Tree *, Tree *);
 Tree *parseProposition(Tree *, Tree *);
